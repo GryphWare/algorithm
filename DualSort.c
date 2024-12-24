47f2e31f63a9b5ebffbeafbde3b16e0057672058
GryphWare <chanhnguyencute12@gmail.com>
2024-12-25 02:27:25 +0700

    DualSort/added error when out of mem

--- DualSort.c
+++ DualSort.c
@@ -5,12 +5,21 @@
 void dualSort(int *arr, int size){
     int *arrSorted = (int*) malloc (size * sizeof(int));
 
+    //check mang ok?
+    if(arrSorted == NULL){
+        perror("Thieu bo nho\n");
+        exit(1);
+    }
+
+    //tao vi tri
     int right = size - 1, left = 0;
 
+    //stop cho den khi 2 diem cham nhau
     while (left <= right){
         int min = INT_MAX, max = INT_MIN;
         int min_index = -1, max_index = -1;
 
+        //tim so lon nhat va nho nhat
         for(size_t i = 0; i < size; i++){
             if(arr[i] != INT_MAX && min > arr[i]){
                 min = arr[i];
@@ -21,17 +30,22 @@ void dualSort(int *arr, int size){
                 max_index = i;
             }
         }
+    //chen so vao day
     arrSorted[left++] = min;
     arrSorted[right--] = max;
 
+    //danh dau mang
     arr[min_index] = INT_MAX;
     arr[max_index] = INT_MAX;
     }
+
+    //in
     for(size_t i = 0; i < size; i++){
         printf("%d ", arrSorted[i]);
     }
+
     printf("\n");
-    free(arrSorted);
+    free(arrSorted);//free bo nho
 }
 
 
